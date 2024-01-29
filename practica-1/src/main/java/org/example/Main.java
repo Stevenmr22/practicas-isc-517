129844becd3ea6c53c1d627930cb647dd4cf9fbb
StevenMateo <Stevenmateo776@gmail.com>
2024-01-29 12:59:57 -0400

    acapite 6

--- practica-1/src/main/java/org/example/Main.java
+++ practica-1/src/main/java/org/example/Main.java
@@ -7,7 +7,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
 import java.util.Scanner;
 
 public class Main {
@@ -39,23 +38,47 @@ public class Main {
             System.out.println("Tipo de contenido: " + contentType);
 
             // Determinar y mostrar el tipo de recurso según el tipo de contenido
-            determineResourceType(contentType);
-
-            // Contar y mostrar la cantidad de líneas del recurso HTML
             if (contentType.contains("text/html")) {
-                int lineCount = countLines(response.body());
-                System.out.println("Cantidad de líneas del recurso HTML: " + lineCount);
+                Document document = Jsoup.parse(response.body());
 
-                // Contar y mostrar la cantidad de párrafos en el documento HTML
-                int paragraphCount = countParagraphs(response.body());
-                System.out.println("Cantidad de párrafos en el documento HTML: " + paragraphCount);
+                // Mostrar información sobre el documento HTML
+                System.out.println("Tipo de recurso: Documento HTML");
+                System.out.println("Cantidad de líneas del recurso HTML: " + response.body().split("\r\n|\r|\n").length);
+                System.out.println("Cantidad de párrafos en el documento HTML: " + document.select("p").size());
 
-                // Contar y mostrar la cantidad de imágenes dentro de los párrafos
-                int imageCount = countImagesInParagraphs(response.body());
+                int imageCount = 0;
+                Elements paragraphs = document.select("p");
+                for (Element paragraph : paragraphs) {
+                    imageCount += paragraph.select("img").size();
+                }
                 System.out.println("Cantidad de imágenes dentro de los párrafos: " + imageCount);
 
-                // Mostrar la cantidad de formularios y detalles de cada formulario
-                countForms(response.body());
+                // Mostrar información sobre los formularios
+                Elements forms = document.select("form");
+                for (int i = 0; i < forms.size(); i++) {
+                    Element form = forms.get(i);
+                    String method = form.attr("method").toUpperCase();
+                    System.out.println("Formulario " + (i + 1) + ":");
+                    System.out.println("  Método: " + method);
+                    Elements inputFields = form.select("input");
+                    for (Element inputField : inputFields) {
+                        String inputType = inputField.attr("type");
+                        System.out.println("  - Campo de tipo input: " + inputType);
+                    }
+                    System.out.println();
+
+                }
+                for (Element f : forms) {
+                    String method = f.attr("method");
+                    if (!method.equalsIgnoreCase("POST")) continue;
+
+                    Document respServer = Jsoup.connect(String.valueOf(url))
+                            .data("asignatura", "practica1")
+                            .header("matricula-id", "10147069")
+                            .post();
+                    System.out.println("Respuesta del servidor: " + respServer.body().text());
+                }
+                System.out.println("Cantidad total de formularios: " + forms.size());
             }
 
         } catch (Exception e) {
@@ -63,73 +86,5 @@ public class Main {
         } finally {
             scanner.close();
         }
-    }
-
-    private static void determineResourceType(String contentType) {
-        // Lógica para determinar el tipo de recurso según el tipo de contenido
-        if (contentType.contains("text/html")) {
-            System.out.println("Tipo de recurso: Documento HTML");
-        } else if (contentType.contains("application/pdf")) {
-            System.out.println("Tipo de recurso: Documento PDF");
-        } else if (contentType.contains("image")) {
-            System.out.println("Tipo de recurso: Imagen");
-        } else {
-            System.out.println("Tipo de recurso: Desconocido");
-        }
-    }
-
-    private static int countLines(String text) {
-        // Contar las líneas en el texto proporcionado
-        return text.split("\r\n|\r|\n").length;
-    }
-
-    private static int countParagraphs(String html) {
-        // Utilizar JSoup para analizar el documento HTML y contar los párrafos
-        Document document = Jsoup.parse(html);
-        Elements paragraphs = document.select("p");
-        return paragraphs.size();
-    }
-
-    private static int countImagesInParagraphs(String html) {
-        // Utilizar JSoup para analizar el documento HTML y contar las imágenes dentro de los párrafos
-        Document document = Jsoup.parse(html);
-        Elements paragraphs = document.select("p");
-        int imageCount = 0;
-
-        for (Element paragraph : paragraphs) {
-            // Contar las imágenes dentro de cada párrafo
-            Elements images = paragraph.select("img");
-            imageCount += images.size();
-        }
-
-        return imageCount;
-    }
-
-    private static void countForms(String html) {
-        // Utilizar JSoup para analizar el documento HTML y contar los formularios
-        Document document = Jsoup.parse(html);
-        Elements forms = document.select("form");
-
-        for (int i = 0; i < forms.size(); i++) {
-            Element form = forms.get(i);
-
-            // Obtener el valor del atributo "method" del formulario
-            String method = form.attr("method").toUpperCase();
-
-            // Mostrar información sobre el formulario
-            System.out.println("Formulario " + (i + 1) + ":");
-            System.out.println("  Método: " + method);
-
-            // Obtener y mostrar campos de tipo input y sus tipos dentro del formulario
-            Elements inputFields = form.select("input");
-            for (Element inputField : inputFields) {
-                String inputType = inputField.attr("type");
-                System.out.println("  - Campo de tipo input: " + inputType);
-            }
-
-            System.out.println(); // Separador entre formularios
-        }
-
-        System.out.println("Cantidad total de formularios: " + forms.size());
     }
 }
