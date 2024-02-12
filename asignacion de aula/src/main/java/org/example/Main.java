116057c86434e3ee3868356281b5af4c17aa5617
StevenMateo <Stevenmateo776@gmail.com>
2024-02-12 12:55:33 -0400

    arreglado fixeo de sesion

--- asignacion de aula/src/main/java/org/example/Main.java
+++ asignacion de aula/src/main/java/org/example/Main.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import io.javalin.Javalin;
+import io.javalin.http.Context;
 import io.javalin.http.staticfiles.Location;
 import org.example.encapsulation.Usuario;
 import java.util.HashMap;
@@ -13,48 +14,46 @@ public class Main {
 
         usuarios.put("admin", new Usuario("admin", "admin"));
 
-    Javalin app = Javalin.create(config ->{
-        //configurando los documentos estaticos.
-        config.staticFiles.add(staticFileConfig -> {
-            staticFileConfig.hostedPath = "/";
-            staticFileConfig.directory = "/public";
-            staticFileConfig.location = Location.CLASSPATH;
-            staticFileConfig.precompress=false;
-            staticFileConfig.aliasCheck=null;
+        Javalin app = Javalin.create(config ->{
+            //configurando los documentos estáticos.
+            config.staticFiles.add(staticFileConfig -> {
+                staticFileConfig.hostedPath = "/";
+                staticFileConfig.directory = "/public";
+                staticFileConfig.location = Location.CLASSPATH;
+                staticFileConfig.precompress=false;
+                staticFileConfig.aliasCheck=null;
+            });
+        }).start(7000);
+
+        app.before("/", ctx -> {
+            if (ctx.sessionAttribute("usuario") == null){
+                ctx.redirect("/login");
+            }
         });
-    }).start(7000);
 
-    app.before("/", ctx -> {
-        if (ctx.cookie("usuario") == null){
-            ctx.redirect("/login");
-        }
-    });
+        app.get("/" , ctx -> {
+            ctx.render("/views/index.html");
+        });
 
-    app.get("/" , ctx -> {
-        ctx.render("/views/index.html");
-    });
+        app.get("/login", ctx -> {
+            if (ctx.sessionAttribute("usuario") != null) {
+                ctx.redirect("/");
+            } else {
+                ctx.render("/views/login.html");
+            }
+        });
 
-    app.get("/login", ctx -> {
-        if (ctx.cookie("usuario") != null) {
-            ctx.redirect("/");
-        } else {
-            ctx.render("/views/login.html");
-        }
-    });
-
-
-    app.post("/login", ctx -> {
-        String usuario = ctx.formParam("usuario");
-        String contrasena = ctx.formParam("contrasena");
-        Usuario user = new Usuario("admin", "admin");
-        if(user.autenticar(usuario, contrasena)){
-            ctx.cookie("usuario", usuario, 120);
-            ctx.redirect("/");
-        }else{
-            ctx.redirect("/login");
-        }
-    });
+        app.post("/login", ctx -> {
+            String usuario = ctx.formParam("usuario");
+            String contrasena = ctx.formParam("contrasena");
+            Usuario user = usuarios.get(usuario);
+            if(user != null && user.autenticar(usuario, contrasena)){
+                ctx.sessionAttribute("usuario", usuario);
+                ctx.redirect("/");
+            }else{
+                ctx.redirect("/login");
+            }
+        });
 
     }
-
 }
